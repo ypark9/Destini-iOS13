c9a500ceea7e3ec12ca08c6484d4987a0d9df108
yoonsoopark <ypark9@ncsu.edu>
2021-01-21 22:35:12 -0500

    finished MVC challenge

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,11 +14,37 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    //controller
+    var storyBrain = StoryBrain()
+    
+    func updateUI() {
+        let curPos = storyBrain.curStoryPos
+        storyLabel.text = storyBrain.storyArray[curPos].title
+        if(choice1Button.titleLabel != nil)
+        {
+            //choice1Button.titleLabel!.text = storyBrain.storyArray[curPos].choice1
+            choice1Button.setTitle(storyBrain.storyArray[curPos].choice1, for: .normal)
+        }
+        if(choice2Button.titleLabel != nil)
+        {
+           // choice2Button.titleLabel!.text = storyBrain.storyArray[curPos].choice2
+            choice2Button.setTitle(storyBrain.storyArray[curPos].choice2, for: .normal)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateUI()
     }
 
-
+    @IBAction func choiceMade(_ sender: UIButton) {
+        if  ((choice1Button.titleLabel) != nil) {
+            
+            // get what next story line we need to prepare
+            storyBrain.getNextStoryNumber(choice: choice1Button.titleLabel!.text!)
+            updateUI()
+        }
+    }
+    
 }
 
--- Destini-iOS13/Model/Story.swift
+++ Destini-iOS13/Model/Story.swift
@@ -7,3 +7,24 @@
 //
 
 import Foundation
+
+struct Story {
+    var title : String
+    var choice1 : String
+    var choice1Destination : Int
+    var choice2 : String
+    var choice2Destination : Int
+    
+//    init(title: String, choice1: String, choice1Destination : Int, choice2 : String,  choice2Destination : Int) {
+//        self.title = title
+//        self.choice1 = choice1
+//        self.choice2 = choice2
+//        self.choice1Destination = choice1Destination
+//        self.choice2Destination = choice2Destination
+//    }
+//    Story(
+//        title: "Your car has blown a tire on a winding road in the middle of nowhere with no cell phone reception. You decide to hitchhike. A rusty pickup truck rumbles to a stop next to you. A man with a wide brimmed hat with soulless eyes opens the passenger door for you and asks: 'Need a ride, boy?'.",
+//        choice1: "I'll hop in. Thanks for the help!", choice1Destination: 2,
+//        choice2: "Better ask him if he's a murderer first.", choice2Destination: 1
+//    )
+}
